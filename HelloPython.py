95c05ea1d7acd3d3c45e61942999b00085e92941
Kraszu <43312997+Kraszu@users.noreply.github.com>
2018-12-04 11:32:34 +0000

    Add files via upload
    My python commit

--- HelloPython.py
+++ HelloPython.py
@@ -3,7 +3,7 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-print ("This is the first time updating...")	
+print("that's Maciej's test")	
 
 
 	
