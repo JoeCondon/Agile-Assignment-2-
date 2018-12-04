97ac549b70b8db1ca914cd051d5569d29b89a5cc
JoeCondon <joe.condon@mycit.ie>
2018-12-04 13:07:04 +0000

    Add files via upload
    patrick and joes next test

--- HelloPython.py
+++ HelloPython.py
@@ -5,7 +5,7 @@ print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 print ("This is the first time updating...")	
 print ("test")
-print("test3")
+print("test4")
 
 
 	
